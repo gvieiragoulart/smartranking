36198c46009a0fec45314351dbb0f6f10992cdde
Gabriel Vieira <gabriel.goulart@tradetechnology.com.br>
2022-12-06 10:44:37 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.3djgsid.mongodb.net/smartranking?retryWrites=true&w=majority'),
+    JogadoresModule,
+    CategoriasModule,
+    DesafiosModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
